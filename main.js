3a5b4182be5c6e6484c4f7f824abf43478e42494
Juan Carlos Martínez Estrada <maej171094@gmail.com>
2024-03-13 18:27:38 -0600

    Display popular movies

--- main.js
+++ main.js
@@ -2,7 +2,49 @@ const global = {
   currentPage: window.location.pathname,
 };
 
-// Fetch data from
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="/src/html/movie-details.html?id=${movie.id}">
+           ${
+             movie.poster_path
+               ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}"/>`
+               : `<img src="/src/images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+           }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch data from TMBD API
+async function fetchAPIData() {
+  const response = await fetch(
+    'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Yjk2MjhkODhlMmVmYjI0YjMwYzcyZGY0ZjkxMWMwMSIsInN1YiI6IjY1ZjIxNDBkZTlkYTY5MDE2MzVlNzNhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xYHzmum2DDmh4l6lz9qStSyqLDU2vOyRGmPhFwjH0I8',
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+}
 
 // Highlight active link
 function highlightActivateLink() {
@@ -20,7 +62,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case 'index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/src/html/shows.html':
       console.log('Shows');
